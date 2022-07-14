1b29c00d032ec8665cd5aaab3fc5248fd9deb83b
ahuyinheng <54666374+ahuyinheng@users.noreply.github.com>
2022-07-14 12:57:15 +0800

    Update train.py

--- train.py
+++ train.py
@@ -28,13 +28,13 @@ if __name__ == "__main__":
     #target_model = get_Model(baseArgs.model)
     #print("ok")
     #print(baseArgs.model)
-    g_model = pgan.Generator(3,512,32)
+    g_model = pgan.Generator(3,512,32)            #load generator
     total_stages = int(math.log2(32/4)) + 1
     for i in range(total_stages-1):
         g_model.grow_network()
         g_model.flush_network()
     g_model = g_model.to(device)
-    target_model = resnet.resnet18().to(device)
+    target_model = resnet.resnet18().to(device)  #load target model
     target_model.load_state_dict(torch.load("./train_model/resnet18_cifar10"))#该模型的精度在cifar10验证集上是91.*%
     tr_transformer = transforms.Compose([
 		transforms.RandomCrop(32, padding=4),  #先四周填充0，在吧图像随机裁剪成32*32
@@ -61,15 +61,15 @@ if __name__ == "__main__":
         transform=te_transformer
 	    #target_transform=Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
 	)
-    test_set = DataLoader(test_data, batch_size=128)
+    test_set = DataLoader(test_data, batch_size=128)    #load dataset
     soft_max = nn.Softmax(dim=1)
     optimizerG = torch.optim.Adam(g_model.parameters(), lr=1e-4, betas=(0.5,0.999))
     fixed_noise = torch.FloatTensor(100, 512).normal_(0.0, 1.0).to(device)
-    epoch = 300
-    for i in range(epoch):  
+    epoch = 300   
+    for i in range(epoch):   #start   
         fake = g_model(fixed_noise) 
         c = correct_cal(result_pre(fake,target_model),t)
         train_generater(fake,g_model,target_model)
         if(c>99.9):
             break
-    my_test(fake,target_model)
+    my_test(fake,target_model)    #check the label of trigger in target model
